5628fce4dcb71ff0a413c2ac4e556dd0990b6018
cnfrank <9570075@qq.com>
2024-02-21 17:04:34 +1300

    PitchDoucment MVC added

---
+++ Models/PitchDocument.cs
@@ -0,0 +1,19 @@
+﻿namespace GoogleAuthDemo.Models
+{
+    public class PitchDocument
+    {
+
+        public int Id { get; set; }
+
+        public string FileName { get; set; }
+
+        public string FileURL { get; set; }
+
+        public PitchDocument(int id, string fileName, string fileURL)
+        {
+            Id = id;
+            FileName = fileName;
+            FileURL = fileURL;
+        }
+    }
+}
